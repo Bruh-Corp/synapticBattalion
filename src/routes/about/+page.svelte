a8089808f226804286c831c92e2487c33331a691
AbbanFahim <lloydtechno58@gmail.com>
2023-07-13 19:00:55 +0400

    added deployment config for github pages

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,47 +1,44 @@
 <script>
-    import '../../fonts.css'
-    import '../../styles.css'
-    import './about.css'
+	import '../../fonts.css';
+	import '../../styles.css';
+	import './about.css';
+	import { base } from '$app/paths';
 </script>
+
 <div class="box">
-    <h1>Synaptic<br>Battalion</h1>
-    <img src="/images/neuron.png" alt="Hi there">
-    <img src="/images/neuron2.png" alt="Hi there">
-    <img src="/images/neuron3.png" alt="Hi there">
-    <img src="/images/neuron4.png" alt="Hi there">
+	<h1>Synaptic<br />Battalion</h1>
+	<img src="{base}/images/neuron.png" alt="Hi there" />
+	<img src="{base}/images/neuron2.png" alt="Hi there" />
+	<img src="{base}/images/neuron3.png" alt="Hi there" />
+	<img src="{base}/images/neuron4.png" alt="Hi there" />
 </div>
 <h2>About Us.</h2>
-<p>Synaptic Battalion (SB.) is an attribute of Bruh Corp. dedicated to editorial and journalistic content.
-    Writers at Synaptic Battalion aim to provide the utmost quality of topical journalism
-    SB. is a publishing site for young and aspiring individuals accentuating and evincing their writing skills.
+<p>
+	Synaptic Battalion (SB.) is an attribute of Bruh Corp. dedicated to editorial and journalistic
+	content. Writers at Synaptic Battalion aim to provide the utmost quality of topical journalism SB.
+	is a publishing site for young and aspiring individuals accentuating and evincing their writing
+	skills.
 </p>
-<h2>
-    Members at SB.
-</h2>
+<h2>Members at SB.</h2>
 
 <div class="content">
-    <div class="contentCard">
-        <img src="/images/talwhite.png" alt="Hi there">
-        <h4>
-            Muhammad Talha Ijaz
-        </h4>
-        <p class="visionary">"Quaint & Pedantic."</p>
-        <div class="date"><span>Apex Journalist</span>Visionary</div>
-    </div>
-    <div class="contentCard">
-        <img src="/images/apple.png" alt="Hi there">
-        <h4>
-            Abban Fahim
-        </h4>
-        <p class="visionary">"Flimsy n' sturdy."</p>
-        <div class="date"><span>Apex Journalist</span>Visionary</div>
-    </div>
-    <div class="contentCard">
-        <img src="/images/fasam.jpg" alt="Hi there">
-        <h4>
-            Fasam Nadeem
-        </h4>
-        <p>"Flimsy n' sturdy."</p>
-        <div class="date"><span>Journalist</span>Member</div>
-    </div>
+	<div class="contentCard">
+		<img src="{base}/images/talwhite.png" alt="Hi there" />
+		<h4>Muhammad Talha Ijaz</h4>
+		<p class="visionary">"Quaint & Pedantic."</p>
+		<div class="date"><span>Apex Journalist</span>Visionary</div>
+	</div>
+	<div class="contentCard">
+		<img src="{base}/images/apple.png" alt="Hi there" />
+		<h4>Abban Fahim</h4>
+		<p class="visionary">"Flimsy n' sturdy."</p>
+		<div class="date"><span>Apex Journalist</span>Visionary</div>
+	</div>
+	<div class="contentCard">
+		<img src="{base}/images/fasam.jpg" alt="Hi there" />
+		<h4>Fasam Nadeem</h4>
+		<p>"Flimsy n' sturdy."</p>
+		<div class="date"><span>Journalist</span>Member</div>
+	</div>
 </div>
+
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import {
     vitePreprocess
 } from '@sveltejs/kit/vite';
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter()
+        adapter: adapter({ pages: "/build" }),
+        paths: {
+            base: dev ? '' : "/synapticBattalion"
+        },
+
     }
 };
 
