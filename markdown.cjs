801be65fb23ec1456bff4078902b2247f52524fc
AbbanFahim <lloydtechno58@gmail.com>
2023-08-30 15:18:40 +0400

    added trial markdown support in writings/trial/

---
+++ markdown.cjs
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const OUTPUT_PATH = 'src/routes/writings/trials';
+
+const articles = fs.readdirSync(OUTPUT_PATH);
+articles.forEach((article) => {
+	let contents = fs.readFileSync(OUTPUT_PATH + '/' + article + '/index.html');
+	fs.writeFileSync(OUTPUT_PATH + '/' + article + '/+page.svelte', contents);
+	fs.rm(OUTPUT_PATH + '/' + article + '/index.html', (e) => {
+		console.error(e);
+	});
+});
---
+++ src/routes/writings/trials/hello/+page.svelte
@@ -0,0 +1,41 @@
+<svelte:head>
+	<title>Bban's elusive article - SB.</title>
+    <link rel="icon" href="{base}/images/comingsoon.jpg" />
+    <meta name="description" content="">
+</svelte:head>
+
+<script>
+	import '../../../../fonts.css';
+	import '../../../../styles.css';
+    import '../../generic-article.css'
+	import { base } from '$app/paths';
+</script>
+
+<h1 class="articletitle">Bban's elusive article</h1>
+
+<div class="datebox">
+    <h4>29th August 2023</h4>
+    <div class="bosx">
+        <h4>By Abbanana</h4>
+        <img src="{base}/images/apple.png" alt="Hi there" />
+    </div>
+</div>
+
+<h2>
+    
+</h2>
+
+<div class="flex">
+    <img class="titleImg" src="{base}/images/comingsoon.jpg" alt="Hi there" />
+</div>
+
+<h3>WHHDHUHUHIUVHIU</h3>
+<h3>bhbffuufuf</h3>
+<h3>hfhihuhgui</h3>
+<p>pruhhhhhh</p>
+<blockquote>
+<p>dfufjjjojo</p>
+</blockquote>
+<p>dudjfjif</p>
+
+
